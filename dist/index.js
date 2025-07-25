06e26540f49393d3153123826aaba8f5bcb4fb04
Akshat <you@example.com>
2025-07-25 13:03:25 +0530

    fix

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
+let allSocket = [];
 let userCount = 0;
 wss.on("connection", (socket) => {
+    allSocket.push(socket);
     userCount = userCount + 1;
     console.log("User connected " + userCount);
     socket.on("message", (event) => {
         console.log("message received: " + event.toString());
-        socket.send("nessage delivered: " + event.toString());
+        for (let i = 0; i < allSocket.length; i++) {
+            const s = allSocket[i];
+            s.send("message from server: " + event.toString());
+        }
+    });
+    socket.on("disconnect", (event) => {
+        allSocket = allSocket.filter(x => x != socket);
     });
 });
