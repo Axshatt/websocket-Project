669b816df1a34a28cbaaebf7736c93a2647c1357
Akshat <you@example.com>
2025-07-26 23:17:48 +0530

    fix:server

--- websocket-fe/src/App.tsx
+++ websocket-fe/src/App.tsx
@@ -1,13 +1,27 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+   const [message,setMessage] = useState(["Hi there","hello","lol"]);
+  useEffect(()=>{
+    const ws = new WebSocket("http://localhost:3000");
+    ws.onmessage=(event)=>{
+      setMessage(m=>[...m,event.data])
+    }
 
+  },[])
 
   return (
     <>
       <div className='h-screen bg-black p-px'>
-        <div className='h-[95vh]'></div>
+        <div className='h-[95vh] mt-[5vh]'>{message.map(message=><div
+         className='m-8'>
+          
+        <span 
+          className="p-4 m-8 rounded bg-white text-black">
+            {message}
+          </span>
+        </div>)}</div>
         <div className="w-full flex  bg-white">
           <input className=" flex-1" type='text' />
           <button className="bg-blue-600 p-4 text-white p-4 cursor-pointer" >Send Message</button>
