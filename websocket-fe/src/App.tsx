a60f7cf63e1a5240aaa410ba8d35a6422030657a
Akshat <you@example.com>
2025-07-26 22:21:59 +0530

    fixed css

--- websocket-fe/src/App.tsx
+++ websocket-fe/src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='h-screen bg-black p-px'>
+        <div className='h-[95vh]'></div>
+        <div className="w-full flex  bg-white">
+          <input className=" flex-1" type='text' />
+          <button className="bg-blue-600 p-4 text-white p-4 cursor-pointer" >Send Message</button>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
